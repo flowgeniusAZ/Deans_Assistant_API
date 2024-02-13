72bd1ef0222e677bdc43801252800a2062ce43c3
flowgeniusAZ <146907293+flowgeniusAZ@users.noreply.github.com>
2024-02-13 10:28:47 -0600

    Update deans_assistant_api.py

--- deans_assistant_api.py
+++ deans_assistant_api.py
@@ -12,6 +12,9 @@ client = OpenAI(api_key='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')
 #Streamlit App Title
 st.title('Dean\'s Assistant API')
 
+#Streamlit image for branding
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQroYsyWjvZmkyguxf2_XUKqcWTNLkZrRbPzPL8MU5I&s', caption='John F Kennedy Middle School')
+
 #Streamlit user input for the prompt
 user_prompt = st.text_area("Enter your question:", "Kid was vaping in the bathroom. what consequences should i give?")
 
