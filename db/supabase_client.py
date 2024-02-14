03e071a53999ec4fcd57af706361e14d6ed310c3
flowgeniusAZ <146907293+flowgeniusAZ@users.noreply.github.com>
2024-02-13 21:00:12 -0600

    Update supabase_client.py

--- db/supabase_client.py
+++ db/supabase_client.py
@@ -1,1 +1,9 @@
 #supabse_client.py
+
+from supabase import create_client, Client
+
+supabase: Client = create_client(url, key)
+
+def upload_embedding(filename: str, filetype: str, embedding):
+    data = {"filename": filename, "filetype": filetype, "embedding": embedding}
+    supabase.table("embeddings").insert(data).execute()
