0e8d87520251e23432c5c0e13ee3be53b9adbc47
flowgeniusAZ <146907293+flowgeniusAZ@users.noreply.github.com>
2024-02-13 21:08:17 -0600

    Update supabase_client.py

--- db/supabase_client.py
+++ db/supabase_client.py
@@ -1,10 +1,16 @@
 #supabse_client.py
 
 from supabase import create_client, Client
-import
+import streamlit as st
 
-url = st.secrets("url
+@st.cache_resource
+
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url,key)
 
 def upload_embedding(filename: str, filetype: str, embedding):
     data = {"filename": filename, "filetype": filetype, "embedding": embedding}
     supabase.table("embeddings").insert(data).execute()
+
