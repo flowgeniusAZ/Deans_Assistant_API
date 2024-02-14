c3fd35d1cb005126b998352e96d7dbcfc1842952
flowgeniusAZ <146907293+flowgeniusAZ@users.noreply.github.com>
2024-02-13 21:11:39 -0600

    Update deans_assistant_api.py

--- deans_assistant_api.py
+++ deans_assistant_api.py
@@ -7,7 +7,7 @@ import time
 import streamlit as st
 
 #Initialize OpenAI Clinet
-client = OpenAI(api_key= OPEN_API_KEY)
+client = OpenAI(api_key= "OPEN_API_KEY")
 
 #Streamlit App Title
 st.title('Dean\'s Assistant')
