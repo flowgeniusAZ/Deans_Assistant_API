f9d724602c29c76f3a2d528d671fe16e49c93913
flowgeniusAZ <146907293+flowgeniusAZ@users.noreply.github.com>
2024-02-15 20:14:13 -0600

    Update deans_assistant_api.py

--- deans_assistant_api.py
+++ deans_assistant_api.py
@@ -45,12 +45,7 @@ if st.button('Submit'):
                 thread_id=thread.id,
                 run_id=run.id
             )
-            time.sleep(5)
-
-            #if run.status == "completed":
-               # message_list = reversed(client.beta.threads.messages.list(
-                    #thread_id=thread.id
-            #))
+            time.sleep(1)
 
             if run.status == 'completed':
                 thread_messages = client.beta.threads.messages.list(thread.id)
@@ -59,23 +54,7 @@ if st.button('Submit'):
                         for content_part in message.content:
                             message_text = content_part.text.value
                             st.markdown(f"**Assistant's Response:** {message_text}")
-        
-            #fetch and display messages
-            #thread_messages = client.beta.threads.messages.list(thread.id)
-           #for message in thread_messages.data:
-                #if message.role == 'assistant':
-                    #for content_part in message.content:
-                        #message_text = content_part.text.value
-                        #st.markdown(f"**Assistant's Response:** {message_text}")
-
- 
- 
-            #message_data = message_list.data
-            #for thread_message in message_data.content:
-                #for message_content in thread_message.content:
-                    #message_text = message_content.text.value
- 
-                    #st.chat_message(thread_message.role).markdown(message_text)
+    
 
 
 # Add a spacer
